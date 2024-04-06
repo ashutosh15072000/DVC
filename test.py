8a4a07a9c0e100a409ac391b79ab325fbead555f
ashutosh15072000 <ashutoshs019@gmail.com>
2024-04-06 10:51:31 +0530

    this is second commit

---
+++ test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+Data=[
+
+    {"name":"ashutosh","age":25,"city":"lucknow"},
+    {"name":"ram","age":15,"city":"delhi"},
+    {"name":"shyam","age":45,"city":"Pune"},
+    {"name":"seeta","age":22,"city":"punjab"}
+]
+
+data=pd.DataFrame(Data)
+
+data.to_csv("data/data.csv",index=False)
